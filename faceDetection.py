e1f018e992d4dff565c9d3b82490b9948fd0e1cd
yansen0508 <36081071+yansen0508@users.noreply.github.com>
2018-03-02 17:58:04 +0100

    Add files via upload

--- faceDetection.py
+++ faceDetection.py
@@ -8,7 +8,7 @@ import numpy as np
 import cv2
 from base import base
 import time
-
+import affine
 class faceDetector:
     def __init__(self):
         
@@ -31,32 +31,43 @@ class faceDetector:
 
     def get_faces(self):
         for (x,y,w,h) in self.faces:
-            cv2.rectangle(self.img,(x,y),(x+w,y+h),(255,0,0),2)
-            roi_gray = self.gray[y:y+h, x:x+w]
-            roi_color = self.img[y:y+h, x:x+w]
-            #eyes = self.eye_cascade.detectMultiScale(roi_gray)
-            #for (ex,ey,ew,eh) in eyes:
-             #   cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
-              
-        for (x,y,w,h) in self.profile:
-            cv2.rectangle(self.img,(x,y),(x+w,y+h),(0,255,0),2)
-            roi_gray = self.gray[y:y+h, x:x+w]
-            roi_color = self.img[y:y+h, x:x+w]
-            #eyes = self.eye_cascade.detectMultiScale(roi_gray)
-            #for (ex,ey,ew,eh) in eyes:
-            #    cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+#            cv2.rectangle(self.img,(x,y),(x+w,y+h),(255,0,0),2)
+            self.img=self.img[x:x+w,y:y+h]
+#            roi_gray = self.gray[y:y+h, x:x+w]
+#            roi_color = self.img[y:y+h, x:x+w]
+#            eyes = self.eye_cascade.detectMultiScale(roi_gray)
+#            for (ex,ey,ew,eh) in eyes:
+#                cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+         
+#        for (x,y,w,h) in self.profile:
+#            cv2.rectangle(self.img,(x,y),(x+w,y+h),(0,255,255),2)
+#            roi_gray = self.gray[y:y+h, x:x+w]
+#            roi_color = self.img[y:y+h, x:x+w]
+#            eyes = self.eye_cascade.detectMultiScale(roi_gray)
+#            for (ex,ey,ew,eh) in eyes:
+#                cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
                 
         return self.img
         
 if __name__=="__main__":
     test=base()
 
-    list_img=test.web_images_from_people_id("337")
+    list_img=test.web_images_from_people_id("334")
     fd=faceDetector()
-    
-    for img in list_img[:5]:
+#    faces=[]
+    faces=np.zeros([len(list_img),224,224,3],dtype=np.uint8)  
+    i=0
+    for img in list_img[:]:
+#        face=cv2.imread(img).astype('uint8') 
         fd.detect_face(img)
         imgf=fd.get_faces()
-        cv2.imshow('img',imgf)
+        imgf=cv2.resize(imgf,(224,224))
+        faces[i]=imgf
+        i=i+1
+#        cv2.imshow('img',imgf)
+#        cv2.waitKey(0)  & 0xFF
+    face_affine=affine.face_alignment(faces)
+    for img in face_affine[:]:
+        cv2.imshow('affine',img)
         cv2.waitKey(0)  & 0xFF
     cv2.destroyAllWindows()
---
+++ face_recog.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar  2 16:29:39 2018
+
+@author: yan
+"""
+import numpy as np
+import cv2  
+import dlib  
+import math
+import matplotlib as plt
+def face_alignment(faces):  
+    ''''' 
+    faces: num * width * height * channels ,value = 0~255, dtype = np.uint8,  
+    note: width must equal to height 
+    '''  
+    print(len(faces))  
+    if len(faces)==4 and faces[0].shape[3]==1:  
+        faces = faces.reshape(faces.shape[:-1]) # if gray, turns to num * width * height, no channel axis 如果是灰度图，去掉最后一维，否则predictor会报错  
+    num = len(faces)  
+      
+    faces_aligned = np.zeros((len(faces),faces[0].shape),dtype=np.uint8)  
+    
+    predictor_path = "./shape_predictor_68_face_landmarks.dat" # dlib提供的训练好的68个人脸关键点的模型，网上可以下  
+    predictor = dlib.shape_predictor(predictor_path) # 用来预测关键点  
+    for i in range(num):  
+        img = faces[i]  
+        rec = dlib.rectangle(0,0,img.shape[0],img.shape[1])  
+        shape = predictor(np.uint8(img),rec) # 注意输入的必须是uint8类型  
+#        print('shape '+shape)
+#        order=[36,45,30,48,54] # left eye, right eye, nose, left mouth, right mouth  注意关键点的顺序，这个在网上可以找  
+#        if show:  
+#            plt.pyplot.figure()  
+#            plt.pyplot.imshow(img,cmap='gray')  
+#            for j in order:  
+#                x = shape.part(j).x  
+#                y = shape.part(j).y  
+#                plt.pyplot.scatter(x,y) # 可以plot出来看看效果，这里我只plot5个点  
+        eye_center =( (shape.part(36).x + shape.part(45).x) * 1./2, # 计算两眼的中心坐标  
+                      (shape.part(36).y + shape.part(45).y) * 1./2)   
+        dx = (shape.part(45).x - shape.part(36).x) # note: right - right  
+        dy = (shape.part(45).y - shape.part(36).y)  
+          
+        angle = math.atan2(dy,dx) * 180. / math.pi # 计算角度  
+#        print angle  
+        
+        RotateMatrix = cv2.getRotationMatrix2D(eye_center, angle, scale=1) # 计算仿射矩阵  
+        RotImg = cv2.warpAffine(img, RotateMatrix, (img.shape[0], img.shape[1])) # 进行放射变换，即旋转  
+        faces_aligned[i] = RotImg  
+    return faces_aligned # uint8  
