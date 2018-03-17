623cac42a3c0a117e3238642079a8450c68e7ac9
helenon_fra <francois.helenon@supelec.fr>
2018-03-17 16:13:43 +0100

    modifié :         traitement.py

--- traitement.py
+++ traitement.py
@@ -23,6 +23,7 @@ from sklearn.metrics import confusion_matrix
 from sklearn.decomposition import PCA
 from sklearn.svm import SVC
 
+from sklearn.cluster import KMeans
 from time import time
 
 from multiprocessing import Pool
@@ -33,50 +34,74 @@ vgg_test=vgg.vgg()
 
 def load_preprocess_data(tup):
     img_path=tup[0]
-    people_id=tup[1]
+    ids=tup[1]
     img=cv2.imread(img_path,1)
-    fd.detect_face(img)
-    imgf=fd.get_faces()
-    
-    if None==imgf:
-        print(" pas de faces détectées : {} ".format(img_path))
-        return
-    else:
-        resized_img=cv2.resize(img,(224,224))
-        face_affine=face_recog.face_alignment(resized_img)[0]
-        
-        return (face_affine ,people_id)
+#    fd.detect_face(img)
+#    imgf=fd.get_faces()
+#    
+#    if None==imgf:
+#        print(" pas de faces détectées : {} ".format(img_path))
+#        return
+#    else:
+#        resized_img=cv2.resize(img,(224,224))
+#        face_affine=face_recog.face_alignment(resized_img)[0]
+#        
+#        return (face_affine ,ids)
+     
+    resized_img=cv2.resize(img,(224,224))
+    return (resized_img ,ids)
 
-    
+def split_array(array,nb_split):
+    n=len(array)
+    l=int(n/nb_split+0.5)
+    return [array[i*l:(i+1)*l] if i<nb_split-1 else array[i*l:] for i in range(nb_split)]
+
+
 
 
 
 t0 = time()
 
-    
-training_data_set=[(img_path,people_id) for track_id,people_id in base_img.tracks.values if people_id!=None for img_path in base_img.track_images_from_track_id(track_id) if img_path!=None ]
-X_train=[]
-y_train=[]
-i=len(training_data_set)
-for batch in np.array_split(training_data_set,number_of_batch):
+
+training_data_set_tracks=[(img_path,(people_id,track_id)) for track_id,people_id in base_img.tracks.values if people_id!=None for img_path in base_img.track_images_from_track_id(track_id) if img_path!=None ]
+X_train_tracks=[]
+y_train_tracks=[]
+i=len(training_data_set_tracks)
+for batch in split_array(training_data_set_tracks,number_of_batch):
     training_set=[]
     with Pool(8) as p:
         training_set=p.map(load_preprocess_data,batch)
     print(i)
     i-=len(batch)
     l=np.array(training_set).transpose()
-    X_train.append(vgg_test.get_img_features(l[0]))
-    y_train.append(l[1])
-  
+    X_train_tracks.append(vgg_test.get_img_features(l[0]))
+    y_train_tracks.append(l[1])
+
 t1=time() 
 print("Loaded and computed features on training data in  %0.3fs" % (t1 - t0))
 
-    
+training_data_set_people=[(img_path,(people_id,None)) for people_id in os.listdir("input_training/people") if people_id!=None for img_path in base_img.web_images_from_people_id(people_id) if img_path!=None ]
+X_train_people=[]
+y_train_people=[]
+i=len(training_data_set_people)
+for batch in split_array(training_data_set_people,number_of_batch):
+    training_set=[]
+    with Pool(8) as p:
+        training_set=p.map(load_preprocess_data,batch)
+    print(i)
+    i-=len(batch)
+    l=np.array(training_set).transpose()
+    X_train_people.append(vgg_test.get_img_features(l[0]))
+    y_train_people.append(l[1])
+
+t2=time() 
+print("Loaded and computed features on training data in  %0.3fs" % (t2 - t1))
+
 test_data_set=[(img_path,None) for track_id in os.listdir("input_testing") for img_path in base_img.track_images_from_track_id(track_id,False) if img_path!=None ]
 X_test=[]
 
 i=len(test_data_set)
-for batch in np.array_split(test_data_set,number_of_batch):
+for batch in split_array(test_data_set,number_of_batch):
     test_set=[]
     with Pool(8) as p:
         test_set=p.map(load_preprocess_data,batch)
@@ -85,10 +110,8 @@ for batch in np.array_split(test_data_set,number_of_batch):
     l=np.array(test_set).transpose()
     X_test.append(vgg_test.get_img_features(l[0]))
     
-
-t2=time() 
-print("Loaded and computed features on testing data in  %0.3fs" % (t2 - t1))
-
+t3=time()
+print("Loaded and computed features on training data in  %0.3fs" % (t3 - t2))
 
 """
 t0 = time()
